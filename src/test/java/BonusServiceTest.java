caf688129baab1b357893be8cb328f56774f1242
GrantGastin <103415118+GrantGastin@users.noreply.github.com>
2022-05-16 10:02:56 +0300

    Correction, adding new tests

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -38,28 +38,27 @@ class BonusServiceTest {
     void shouldCalculateFornoRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
         // подготавливаем данные:
-        long amount = 1000_60;
-        boolean registered = true;
+        long amount = 100_60;
+        boolean notregistered = true;
         long expected = 30;
 
         // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
+        long actual = service.calculate(amount, notregistered);
 
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
 
     @org.junit.jupiter.api.Test
-    void shouldCalculateFornoRegisteredAndOverLimit() {
+    void shouldCalculateForotRegisteredAndOverLimit() {
         BonusService service = new BonusService();
-
         // подготавливаем данные:
-        long amount = 1_000_000_60;
-        boolean registered = true;
+        long amount = 1_000_60;
+        boolean notregistered = true;
         long expected = 500;
 
         // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
+        long actual = service.calculate(amount, notregistered);
 
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
